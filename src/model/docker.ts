85008dd47f6f27b5d63b3ec2b194694414fbe7a8
mattia <mattia.belletti@gmail.com>
2023-10-14 18:38:15 +0200

    try network host

--- src/model/docker.ts
+++ src/model/docker.ts
@@ -90,6 +90,7 @@ class Docker {
 
     return `docker run \
             --workdir ${dockerWorkspacePath} \
+            --network host \
             --rm \
             ${ImageEnvironmentFactory.getEnvVarString(parameters)} \
             --env UNITY_SERIAL="${unitySerial}" \
